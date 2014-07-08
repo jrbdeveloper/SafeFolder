a151761d601b5d42a1f2d6673b46017814812314
John Bales <jrbdeveloper@gmail.com>
2014-07-07 18:21:16 -0700

    adding untracked files

---
+++ WindowsFormsApplication1/EncryptForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class EncryptForm : Form
+    {
+        public EncryptForm()
+        {
+            InitializeComponent();
+        }
+
+        public CheckedListBox EmailList
+        {
+            get
+            {
+                return lstRecipients;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lstRecipients.Items.Add(txtEmailAddress.Text, true);
+            txtEmailAddress.Text = "";
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ WindowsFormsApplication1/EncryptionPreferencesManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication1
+{
+    public class EncryptionPreferencesManager
+    {
+        private static System.Timers.Timer _elapseTimer;
+        public List<string> emailList = new List<string>();
+        public static EncryptForm EncryptPreferencesForm = null;
+        
+
+
+        public List<string> ShowEncryptForm()
+        {           
+            try
+            {
+                if (EncryptPreferencesForm == null)
+                {
+                    EncryptPreferencesForm = new EncryptForm();
+
+                    EncryptPreferencesForm.ShowDialog();
+                    if (EncryptPreferencesForm.DialogResult == DialogResult.OK)
+                    {
+                        emailList = (from string e in EncryptPreferencesForm.EmailList.CheckedItems
+                                     select e).ToList<string>();
+
+                    }
+                    SetupTimer();
+                }
+                else
+                {
+                    ResetTimer();
+                }
+
+            }
+            catch
+            {
+                System.Diagnostics.Debugger.Break();
+            }
+            finally
+            {
+                //if (EncryptPreferencesForm != null)
+                //{
+                //    EncryptPreferencesForm.Dispose();
+                //    EncryptPreferencesForm = null;
+                //}
+            }
+            return emailList;
+        }
+
+        private void SetupTimer()
+        {
+            _elapseTimer = new System.Timers.Timer(10000);
+
+            // Hook up the event handler for the Elapsed event.
+            _elapseTimer.Elapsed += new System.Timers.ElapsedEventHandler(OnPreferencesTimeOut);
+
+            // Only raise the event the first time Interval elapses.
+            _elapseTimer.AutoReset = false;
+            _elapseTimer.Enabled = true;
+        }
+
+        private void ResetTimer()
+        {
+            _elapseTimer.Enabled = false;
+            _elapseTimer.Elapsed -= new System.Timers.ElapsedEventHandler(OnPreferencesTimeOut);
+            _elapseTimer.Dispose();
+            SetupTimer();
+        }
+
+        private void OnPreferencesTimeOut(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            emailList = null;
+            if (EncryptPreferencesForm != null)
+            {
+                EncryptPreferencesForm.Dispose();
+                EncryptPreferencesForm = null;
+            }
+        }
+    }
+}
